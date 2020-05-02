2587dd9528bb154fdb77f63c7d818e5d97dd5446
WanderS1902 <64294606+WanderS1902@users.noreply.github.com>
2020-05-02 19:34:50 -0400

    mi primera commit

---
+++ tu acosador secreto.js
@@ -0,0 +1,13 @@
+//esto nos manda una alerta en pantalla; aler (nombre;
+//var nombre = prompt("ingresa tu nombre");
+//var apellido = prompt ("Ingresa tu apellido")
+//var edad = prompt("ingresa tu edad");
+//nombre = prompt("ingresa tu nombre", "nombre"); esta es la funcion para guardar los nombres. por eso las funciones casi siempre se almacenan en una variable
+//document.write(nombre + " " + apellido + " " + edad);
+//if(edad > 18){
+    //document.write(" " + "Puedes pasar");}
+//else{
+    //document.write(" " + "No pasas, no lo siento");}
+
+
+    
